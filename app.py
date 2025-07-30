e0b3b02baf1e16edc0a370edeb48e6ac157dc7ab
Tanishq Kolhatkar <91214235+tanishqkolhatkar93@users.noreply.github.com>
2025-07-30 19:50:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,19 @@ def home():
 
 
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root', # <--- IMPORTANT: Replace with your MySQL username
-    'password': '12345', # <--- IMPORTANT: Replace with your MySQL password
-    'database': 'quiz_app_db' # <--- IMPORTANT: Ensure this database exists or create it
+    'host': 'database-1.cjm4csiy6e1f.eu-north-1.rds.amazonaws.com',  # ✅ NOT 'localhost'
+    'user': 'root',
+    'password': '12345',
+    'database': 'quiz_app_db'
 }
+
+
+# DB_CONFIG = {
+#    'host': 'localhost',
+#    'user': 'root', # <--- IMPORTANT: Replace with your MySQL username
+#    'password': '12345', # <--- IMPORTANT: Replace with your MySQL password
+#    'database': 'quiz_app_db' # <--- IMPORTANT: Ensure this database exists or create it
+# }
 
 UPLOAD_FOLDER = 'uploads'
 if not os.path.exists(UPLOAD_FOLDER):
